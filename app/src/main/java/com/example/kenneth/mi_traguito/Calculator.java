5d3d01856f8fcef85fc5c9580db31372716ff4de
xlKennethlx <kenneth.velez2@upr.edu>
2017-03-23 14:13:39 -0300

    prueba

--- app/src/main/java/com/example/kenneth/mi_traguito/Calculator.java
+++ app/src/main/java/com/example/kenneth/mi_traguito/Calculator.java
@@ -23,8 +23,7 @@ public class Calculator extends AppCompatActivity {
         EditText cantidad_ingresada = (EditText)findViewById(R.id.editText4);
         EditText porciento_ingresado = (EditText)findViewById(R.id.editText5);
         TextView t1 = (TextView)findViewById(R.id.textView);
-
-        //kenneth pato
+        //probando
 
         double P = Double.parseDouble(porciento_ingresado.getText().toString());//Porciento de alcohol en la bebida
         double onzas = Double.parseDouble(onzas_ingresadas.getText().toString());//onzas de la bebida
